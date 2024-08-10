f6ecc674429946f1a8a2ca794c7b0bfed61d4545
DevJaysz <149165379+DevJaysz@users.noreply.github.com>
2024-08-10 14:43:49 +0800

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-export const content = [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {
-    backgroundImage: {
-      'custom-background': "url('./src/assets/background/325489479_1212154686107383_7850534301493123193_n.jpg')",
-    },
-    fontFamily: {
-      'century-gothic': ['century-gothic', 'san-serif'] // Adding your custom font
-      // Add other font families as needed
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        century:['century-gothic']
+      }
     },
   },
-};
-export const plugins = [];
+  plugins: [],
+}
