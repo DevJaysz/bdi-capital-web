e913e0d530645c2615a02cc416d01f7ae0666317
DevJaysz <149165379+DevJaysz@users.noreply.github.com>
2024-08-10 13:31:17 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-background': "url('./src/assets/background/325489479_1212154686107383_7850534301493123193_n.jpg')",
+        'custom-background': "url('./public/background/325489479_1212154686107383_7850534301493123193_n.jpg')",
       },
       fontFamily: {
         gothic: ['century-gothic', 'san-serif'] // Adding your custom font
